de14a5340fb43f19c3d429b15b3e48ba3a88932f
igor <igor_yalta@rambler.ru>
2016-02-01 18:41:02 +0200

    добавил

--- students/views.py
+++ students/views.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from django.shortcuts import render
  
 # Create your views here.
@@ -13,8 +15,35 @@ from django.http import HttpResponse
 
 
 # Views for Students
+# def students_list(request):
+# 	return render(request, 'students/students_list.html', {})
 def students_list(request):
-	return render(request, 'students/students_list.html', {})
+	students = (
+		{'id': 1,
+		 'first_name': u'Віталій',
+		 'last_name': u'Подоба',
+		 'ticket': 235,
+		 'image': 'img/1.jpg'},
+		{'id': 2,
+		 'first_name': u'Корост',
+		 'last_name': u'Андрій',
+		 'ticket': 2123,
+		 'image': 'img/2.jpg'},
+		{'id': 3,
+		 'first_name': u'Остов',
+		 'last_name': u'Степан',
+		 'ticket': 2123,
+		 'image': 'img/3.jpg'},
+	)
+	return render(request, 'students/students_list.html',
+		{'students': students})
+
+
+
+
+
+
+
 def students_add(request):
 	return HttpResponse('<h1>Student Add Form</h1>')
 def students_edit(request, sid):
