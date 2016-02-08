4fac6876578f54af66eea5bafbbe61c6f7adecc9
igor <igor_yalta@rambler.ru>
2016-02-08 23:11:06 +0200

    добавил

--- git.sh
+++ git.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
+git status
+echo "OK 1-4 status"
 git add *
-echo "OK 1-3"
+echo "OK 2-4 add"
 git commit -a -m 'добавил'
-echo "OK 2-3"
+echo "OK 3-4 commit"
 git push
-echo "OK 3-3"
+echo "OK 4-4 push"
 exit
