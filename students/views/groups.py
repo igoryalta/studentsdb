23096d9d184405a9578677114f53024df53a91ee
igor <igor_yalta@rambler.ru>
2016-02-16 00:17:55 +0200

    добавил

--- students/views/groups.py
+++ students/views/groups.py
@@ -14,21 +14,70 @@ from django.http import HttpResponse
 # 		return HttpResponse('<h1>Hello Word</h1>')
 
 
-# Views for Groups
+
+# # Views for Groups
+# def groups_list(request):
+# 	groups = (
+# 		{'id': 1,
+# 		 'groups_name': u'МтМ-21',
+# 		 'star_name': u'Ячменев Олег'},
+# 		{'id': 2,
+# 		 'groups_name': u'МтМ-22',
+# 		 'star_name': u'Онов Дмитро'},
+# 		{'id': 3,
+# 		 'groups_name': u'МтМ-23',
+# 		 'star_name': u'Чамин Степан'},
+# 	)	
+# 	return render(request, 'students/groups_list.html',
+# 		{'groups': groups})
+
+
+from ..models import Group
+
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
 def groups_list(request):
-	groups = (
-		{'id': 1,
-		 'groups_name': u'МтМ-21',
-		 'star_name': u'Ячменев Олег'},
-		{'id': 2,
-		 'groups_name': u'МтМ-22',
-		 'star_name': u'Онов Дмитро'},
-		{'id': 3,
-		 'groups_name': u'МтМ-23',
-		 'star_name': u'Чамин Степан'},
-	)	
+	groups = Group.objects.all()
+
+	# try to order groups list
+	
+	order_by = request.GET.get('order_by', '')
+	
+	if order_by == '':
+		order_by = 'title'
+		st_def = '1'
+	else:
+		st_def ='0'	
+    #	комент
+		# получаем в GET запросе параметр order_by, когда пользователь щелкнет по ссылке 
+		#  <a href={% url "home" %}?order_by=last_name>Фамилия&uarr;</a> 
+		# GET вернет нам в order_by - last_name or first_name or ticket
+		# параметр запроса сразу за знаком ?
+		# если пользователь не щелкал по ссылке, то order_by - пустой, 
+		# и мы присваиваем ему значение 'last_name'
+		# это мы устанавливаем сотрировку вывода по умолчанию по этому полю
+	if order_by in ('title', 'leader', 'id'):
+		groups = groups.order_by(order_by)
+		if request.GET.get('reverse', '') == '1':
+			groups = groups.reverse()
+
+	# paginate groups
+	paginator = Paginator(groups, 3)
+	page = request.GET.get('page')
+	try:
+		groups = paginator.page(page)
+	except PageNotAnInteger:
+		# If page is not an integer, deliver first page.
+		groups = paginator.page(1)
+	except EmptyPage:
+		# If page is out of range (e.g. 9999), deliver
+		# last page of results.
+		groups = paginator.page(paginator.num_pages)
+
+
 	return render(request, 'students/groups_list.html',
-		{'groups': groups})
+		{'groups': groups, 'st_def' : st_def})
+
 	
 def groups_add(request):
 	return HttpResponse('<h1>Group Add Form</h1>')
